facb5d37a1cdb6efb26aba20f6bfe76cdc84bcd1
Arnstein Ressem <aressem@yahoo-inc.com>
2017-08-29 13:11:09 +0200

    Use latest version when building latest and the last rpm tag.

--- bin/tag-and-push-release.sh
+++ bin/tag-and-push-release.sh
@@ -10,7 +10,5 @@ fi
 
 readonly VERSION=$1
 
-sed -i.orig "s/^RUN.*yum.*install.*vespa-[0-9]\.[0-9]*\.[0-9]*.*/RUN yum install -y vespa-$VERSION/" Dockerfile
-git commit -am "Updated version to $VERSION"
 git tag -a -m "Release $VERSION" $VERSION
 git push --follow-tags
